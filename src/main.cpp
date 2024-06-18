c6513cf7d469ec46df01dbdf626f947d6dc3bb14
IndunilThushara <i8thushara.edu@gmail.com>
2024-06-18 23:20:12 +0530

    Defined functions to notify user about error in OLED display and to initialized the OLED display with retry mechanism.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,44 @@
 #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C // Address found using I2C scanner **** can be 0x3D, so switch if it isn't working.
 
+// LED Indicators
+#define DisplayErrorLED 12 // Indicate the errors occured on the display
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Object in Adafruit_SSD1306 class
+
+// Function to notify user of an error in OLED display initialization process.
+void notifyUserAboutDisplayError()
+{
+  // Blink an LED to notify the display error & send a message to a connected app
+  pinMode(DisplayErrorLED, OUTPUT);
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(DisplayErrorLED, HIGH);
+    Serial.println("Warning: A Error in display Initialization...Check the connections.");
+    delay(500);
+    digitalWrite(DisplayErrorLED, LOW);
+    delay(500);
+  }
+}
+
+// function for initialize the OLED display with retry mechanism.
+bool initDisplay()
+{
+  for (int attempts = 0; attempts < 3; attempts++)
+  {
+    if (display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+      Serial.println("Display initialization is successful");
+      return true; // initialization is successful.
+    }
+
+    notifyUserAboutDisplayError(); // notify about error
+    delay(1000);
+  }
+
+  Serial.println("Display initialization is failed!");
+  return false; // Initialization failed after 3 attempts
+}
 
 void setup()
 {
