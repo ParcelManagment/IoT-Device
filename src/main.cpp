0d787ba7c990748c495c0879fc54cd3325ba739e
IndunilThushara <i8thushara.edu@gmail.com>
2024-06-18 22:42:39 +0530

    Add required libaries for I2C,OLEDDisplay and watchdoctimer. defined display dimensions. Created an display object.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <esp_task_wdt.h>
+
+// OLED display dimensions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64    // or 32 for smaller display ****should be chnage.
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // Address found using I2C scanner **** can be 0x3D, so switch if it isn't working.
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Object in Adafruit_SSD1306 class
 
 void setup()
 {
